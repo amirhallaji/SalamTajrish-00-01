ab3d0dbdf4919941cab557f25853d233cd9d95fb
amirhallaji <a.hallaji.b@gmail.com>
2021-08-22 09:58:39 +0430

    inputs added.

---
+++ 31mordad/input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+      int width, length;
+      scanf("%d%d", &width, &length);
+      int area = width * length;
+      printf("area is: %d", area);
+      return 0;
+}
---
+++ 31mordad/input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+      double length, width;
+      cin >> length >> width;
+      cout << length * width;
+      return 0;
+}
