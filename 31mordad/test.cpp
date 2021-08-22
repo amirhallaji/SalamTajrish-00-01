80b3c426e800f3d1145bb0b6c870ae3bd635932a
amirhallaji <a.hallaji.b@gmail.com>
2021-08-22 09:48:37 +0430

    Type casting addded.

---
+++ 31mordad/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+      double a = 13.4, b = 45.987;
+      int c;
+      c = b / a;
+
+      cout << int(b / a) << endl;
+      cout << c;
+      return 0;
+}
