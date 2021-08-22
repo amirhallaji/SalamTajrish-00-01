c7f8f0b7caa4001a1650f8db7f6e8efa796a8571
amirhallaji <a.hallaji.b@gmail.com>
2021-08-22 09:23:37 +0430

    some files added.

---
+++ 31mordad/area.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+      double radius = 5.67, area;
+      area = radius * radius * 3.14;
+      printf("area is: %lf", area);
+      return 0;
+}
---
+++ 31mordad/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main() {
+      int a = 3, b = 5;
+      char ch = '^';
+      printf("a is : %d and b is: %d\n", a, b);
+      printf("ch is: %c", ch);
+      return 0;
+}
