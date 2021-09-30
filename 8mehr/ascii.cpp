a324ac80aaf9e60613059d618bcad23bf0d96bf6
amirhallaji <a.hallaji.b@gmail.com>
2021-09-30 14:15:56 +0330

    ascii.cpp added.

---
+++ 8mehr/ascii.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+      char ch = 'a';
+      cout << (int)ch << endl;
+
+      int te = 200;
+      cout << (char)te << endl;
+      return 0;
+}
