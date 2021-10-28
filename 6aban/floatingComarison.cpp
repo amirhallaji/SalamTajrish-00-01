6fe9e18e75ef0e041c01c1109128a36d9dd9b586
amirhallaji <a.hallaji.b@gmail.com>
2021-10-28 14:42:32 +0330

    feat: floatingComparison added.

---
+++ 6aban/floatingComarison.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+      double n = 0;
+      while(n < 1.01){
+            cout << n << " ";
+            n += 0.1;
+      }     
+      return 0;
+}
